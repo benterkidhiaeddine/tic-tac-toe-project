54f6d5d0123ebd590431062ea059b60572abcca5
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-06-03 17:00:40 +0100

    Create the functionality with altering moves

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const gameContainer = document.getElementById("game-container");
+
 //create  a game board array ,since a gameBoard is only singular we are going to create one single instance of it with module pattern
 
 const gameBoard = (function () {
-  const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+  const gameBoardArray = [];
+
+  //empty board at the beginning
+  for (let i = 0; i < 9; i++) {
+    gameBoardArray.push({
+      clicked: false,
+      symbol: "",
+    });
+  }
   let currentPlayerTurn = "X";
 
-  //create a function that takes the a number , index of the array and updates it with the current player symbol
+  //create square element function
 
-  function updateGameBoard(squareId) {
-    gameBoardArray[squareId] = currentPlayerTurn;
+  function createSquareElement(square, index) {
+    const squareElement = document.createElement("div");
+    squareElement.dataset.indexID = index;
+    squareElement.innerText = square.symbol;
+    squareElement.classList.add("square");
+    gameContainer.appendChild(squareElement);
   }
 
   //create a function that updates the current player state
@@ -21,12 +35,35 @@ const gameBoard = (function () {
   //create a function that renders the gameBoard
 
   function renderGameBoard() {
-    gameBoardArray.map((symbol, index) => {});
+    gameContainer.innerHTML = "";
+    gameBoardArray.map(createSquareElement);
+
+    const squares = document.querySelectorAll(".square");
+
+    for (let square of squares) {
+      square.addEventListener("click", handlePlay);
+    }
+  }
+
+  function handlePlay(e) {
+    console.log(e);
+    console.log("click being handled");
+
+    const clickedSquareIndex = e.target.dataset.indexID;
+    //if the square was already clicked return
+    if (gameBoardArray[clickedSquareIndex].clicked) {
+      return;
+    }
+    gameBoardArray[clickedSquareIndex].symbol = currentPlayerTurn;
+    gameBoardArray[clickedSquareIndex].clicked = true;
+    updateCurrentPlayerTurn();
+    renderGameBoard();
   }
 
   return {
     gameBoardArray,
     updateCurrentPlayerTurn,
+    renderGameBoard,
   };
 })();
 
@@ -36,13 +73,15 @@ const playerFactory = (symbol, isMyTurn) => {
   //create a function that updates if it's the player turn or no
 
   function updateTurn() {
-    this.isMyTurn = true;
+    this.isMyTurn ? (this.isMyTurn = false) : (this.isMyTurn = true);
   }
 
   return Object.assign({}, { symbol }, { isMyTurn }, { updateTurn });
 };
 
-let player1 = playerFactory("X", true);
-let player2 = playerFactory("O", false);
+let firstPLayer = playerFactory("X", true);
+let secondPlayer = playerFactory("O", false);
 
 console.log(gameBoard.gameBoardArray);
+
+gameBoard.renderGameBoard();
