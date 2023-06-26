a2ef9bf5d8454fa49c0ba1e2d2a6f51075c6f4d7
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-06-26 16:22:45 +0100

    Make it possible to give players names before starting the game

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+//different container variables
+
+const appContainer = document.getElementById("app-container");
 const gameContainer = document.getElementById("game-container");
+const boardContainer = document.getElementById("board-container");
+
+const addPLayerFormContainer = document.getElementById(
+  "add-player-form-container"
+);
+
+/// the app Container will contain only the form that input the name of the players first
+
+gameContainer.style.display = "none";
+
+// then after player names are inputed the add player form container will be replaced by the game container
 
 //create  a game board array ,since a gameBoard is only singular we are going to create one single instance of it with module pattern
 
@@ -34,7 +48,7 @@ const gameBoard = (function () {
   //populate the game Board at the begining
   resetGameBoard();
 
-  //create a function that return winning squares if there winning squares
+  //create a function that return winning squares if there winning squares else it returns null
   function winningSquares() {
     const winingLines = [
       [0, 1, 2],
@@ -61,7 +75,7 @@ const gameBoard = (function () {
     return null;
   }
 
-  //create square element function this
+  //create square element and adds it to the game Container
 
   function createSquareElement(square, index) {
     const squareElement = document.createElement("div");
@@ -69,7 +83,7 @@ const gameBoard = (function () {
     squareElement.dataset.indexID = index;
     squareElement.innerText = square.symbol;
     squareElement.classList.add("square");
-    gameContainer.appendChild(squareElement);
+    boardContainer.appendChild(squareElement);
   }
 
   //create a function that updates the current player state
@@ -84,7 +98,7 @@ const gameBoard = (function () {
 
   function renderGameBoard() {
     //empty the board
-    gameContainer.innerHTML = "";
+    boardContainer.innerHTML = "";
     //fill the board with squares
     gameBoardArray.map(createSquareElement);
   }
@@ -130,14 +144,38 @@ const playerFactory = (symbol, name) => {
   return { symbol, name };
 };
 
-let firstPLayer = playerFactory("X", "Dhia");
-let secondPlayer = playerFactory("O", "Taybou");
+//select the input elements in the form
 
-gameBoard.addPlayers(firstPLayer, secondPlayer);
+const player1Input = document.getElementById("player-1");
+const player2Input = document.getElementById("player-2");
 
-console.log(gameBoard.gameBoardArray);
+///select the submit button of the player names
+
+const submitNames = document.getElementById("submit-names");
+
+//add the functionality when the submit names button is clicked get the names and create player objects and pass them to add players functions and allow the game to start
+
+submitNames.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //get player names from inputs
+  const player1Name = player1Input.value;
+  const player2Name = player2Input.value;
+
+  // create player objects
+  let firstPLayer = playerFactory("X", player1Name);
+  let secondPlayer = playerFactory("O", player2Name);
+
+  //add the player to the game board
+  gameBoard.addPlayers(firstPLayer, secondPlayer);
+
+  addPLayerFormContainer.style.display = "none";
+  gameContainer.style.display = "block";
+});
 
 gameBoard.renderGameBoard();
+
+//reset the board  button functionality -----------------------------------------------------------------------------------------------------------------------
 
 const resetGameButton = document.getElementById("reset-game");
 
@@ -145,3 +183,7 @@ resetGameButton.addEventListener("click", () => {
   gameBoard.resetGameBoard();
   gameBoard.renderGameBoard();
 });
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+//functionality for the player to enter their names
